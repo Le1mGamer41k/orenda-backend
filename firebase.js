1dacf3e0cbfcce057441edf7ad438564ab78db22
Vladyslav Buhil <vladyslav.buhil.oi.2023@lpnu.ua>
2025-06-03 23:23:56 +0300

    fix: use FIREBASE_SERVICE_ACCOUNT from env

--- firebase.js
+++ firebase.js
@@ -1,5 +1,12 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
+const serviceAccountJSON = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+if (!serviceAccountJSON) {
+    throw new Error("❌ FIREBASE_SERVICE_ACCOUNT не задано у середовищі.");
+}
+
+const serviceAccount = JSON.parse(serviceAccountJSON);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
